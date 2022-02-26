726b7fd7a29c3b3a743502889a107d5ccf840abb
Adnan Shaukat <adnanshaukat425@gmail.com>
2022-02-26 23:37:35 +0500

    Added models project

---
+++ IMS/Models/SQL/ApplicationSQLDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.SQL
+{
+    internal class ApplicationSQLDBContext : DbContext
+    {
+        public ApplicationSQLDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
